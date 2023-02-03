0b743c2ebd25e67006f65838896ce7f1f810cde6
wolves <yangwolves@foxmail.com>
2023-02-03 19:23:02 +0800

    git

---
+++ README.md
@@ -0,0 +1,9 @@
+# GITPUSH
+- auto run git add, commit and push
+- 自动允许git命令（add,commit,push)
+- The file ```.gitignore``` must exists
+- 文件```.gitignore```必须存在
+- git remote all must exists
+- 远程仓库 all 必须存在
+- git remote add all <>
+- git remote set-url --add all <>
--- main.cpp
+++ main.cpp
@@ -1,9 +1,45 @@
-#include<iostream>
+#include "stdio.h"
+#include "unistd.h"
+#include "string.h"
 #include <stdlib.h>
+#include <iostream>
+
 using namespace std;
 
-int main()
+// 最后回传的绝对路径名称结尾没有'/'
+bool getAbsolutePath(char* absolutePath, const char *execName)
 {
-	system("time");
-	return 0;
+    if(*(execName) == '/')
+    {
+        sprintf(absolutePath, "%s", execName);
+    }
+    else
+    {
+        char temp[1024];
+        getcwd(temp, 4096);
+        sprintf(absolutePath, "%s/%s", temp, execName);
+    }
+    char *ptr = strrchr(absolutePath, '/');
+    *ptr = '\0';
+    return true;
+}
+
+int main(int argc, char *argv[])
+{
+    char absolutePath[128];
+    getAbsolutePath(absolutePath, argv[0]);
+    
+	char *ig = "/.gitignore";
+
+	strcat(absolutePath,ig);
+
+	int check = access(absolutePath,F_OK);
+
+    if(check == 0){
+		system("git add .");
+		system("git commit -m 'git'");
+		system("git push all");
+	}
+
+    return 0;
 }
